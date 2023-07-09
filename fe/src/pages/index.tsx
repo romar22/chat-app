c5349d3d1733ac71a1f24dd30bb27c89ab65569a
romar22 <romardesabille@gmail.com>
2023-07-09 13:44:18 +0800

    login style

--- fe/src/pages/index.tsx
+++ fe/src/pages/index.tsx
@@ -1,9 +1,3 @@
-import { Inter } from 'next/font/google';
-
-const inter = Inter({ subsets: ['latin'] })
-
-import { useUser } from "@/context/UserContext";
-import { useState } from "react"
 import { AxiosError, AxiosResponse } from 'axios';
 import { useRouter } from 'next/router';
 import { getToken } from '@/shared/api/user';
@@ -15,6 +9,10 @@ import { TOKEN } from '@/shared/config';
 import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from 'yup';
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+import { Loader2 } from 'lucide-react';
 
 const schema = yup.object().shape({
     username: yup.string().required(),
@@ -26,17 +24,14 @@ export default function Login(){
     const { 
         mutate ,
         isLoading,
-    } = useMutation<AxiosResponse<Token>, AxiosError<any>, Credentials>(
-        getToken
-    );
-    const {
-        register,
-        handleSubmit,
-        setError,
-        formState: { errors },
-    } = useForm<Credentials>({
+    } = useMutation<AxiosResponse<Token>, AxiosError<any>, Credentials>(getToken);
+
+    const form = useForm<Credentials>({
         resolver: yupResolver(schema),
-        mode: "onTouched",
+        defaultValues: {
+            username: "",
+            password: "",
+        }
     });
 
     function onSubmit(credentials: Credentials){
@@ -45,13 +40,14 @@ export default function Login(){
                 setAxiosAuthInstanceHeader(resp.data.access);
                 Cookies.set(TOKEN.ACCESS, resp.data.access);
                 Cookies.set(TOKEN.REFRESH, resp.data.refresh);
-                router.push('/u/u/');
+                router.push("/u/u/");
             },
             onError: (error) => {
-                if(error.code === 'ERR_NETWORK'){
-                    setError("password", { type: "manual", message: 'Server Down' });
+                if(error.code === "ERR_NETWORK"){
+                    form.setError("password", { type: "manual", message: "Server Down" });
                 }else{
-                    setError("password", {
+                    form.setError("username", { type: "manual", message: "" });
+                    form.setError("password", {
                         type: "manual",
                         message: error.response?.data?.non_field_errors[0]
                     });
@@ -61,17 +57,52 @@ export default function Login(){
     }
 
     return (
-        <div>
-            {isLoading? 'loading...': ''}
-            <form onSubmit={handleSubmit(onSubmit)} noValidate>
-                <h1>Login</h1>
-                <input {...register("username")} />
-                <div>{errors.username?.message}</div>
-                <br/>
-                <input {...register("password")} autoComplete="true" type="password" />
-                <div>{errors.password?.message}</div>
-                <button type="submit">Login</button>
-            </form>
+        <div className="w-11/12 max-w-md m-auto pt-44">
+            <Form {...form}>
+                <form onSubmit={form.handleSubmit(onSubmit)} 
+                      className="flex-grow space-y-5"
+                >
+                    <div className="text-center">
+                        <h1 className="text-4xl">
+                            Sign in to your account
+                        </h1>
+                        {/* <small>Click to enable darkmode</small> */}
+                    </div>
+                    <FormField
+                        control={form.control}
+                        name="username"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Username</FormLabel>
+                                <FormControl>
+                                    <Input placeholder="Enter your username" {...field} />
+                                </FormControl>
+                                <FormMessage className="font-normal" />
+                            </FormItem>
+                        )}
+                    />
+                    <FormField
+                        control={form.control}
+                        name="password"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Password</FormLabel>
+                                <FormControl>
+                                    <Input placeholder="Enter your password" {...field} />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                    <Button 
+                        disabled={isLoading}
+                        type="submit"
+                    >
+                        {isLoading && <Loader2 className="mr-2 h-4 w-4 animate-spin" />}
+                        {isLoading ? 'Logging In..': 'Login'}
+                    </Button>
+                </form>
+            </Form>
         </div>
     )
 }
